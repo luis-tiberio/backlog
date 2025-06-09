df46da1d0f30a48de9ddd52168b6c8985a3c5c3f
luis-tiberio <luisotaviotiberio@gmail.com>
2025-06-09 12:54:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ timezone = pytz.timezone('America/Sao_Paulo')
 
 # Diretório de download para GitHub Actions
 download_dir = "/tmp"
-
-# Cria o diretório, se não existir
 os.makedirs(download_dir, exist_ok=True)
 
 def login(page):
@@ -31,28 +29,59 @@ def login(page):
 def get_data(page):
     data = []
     try:
-        # Primeiro link
-        page.goto("https://spx.shopee.com.br/#/dashboard/facility-soc/historical-data")
-        page.wait_for_selector('xpath=//*[@id="mgmt-dashboard-content"]/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/canvas', timeout=45000)
-        first_value = page.inner_text('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div[1]/div/div/div[2]/div[2]/div[2]/div/div/div/table/tbody/tr[2]/td[25]')
+        d1 = 'SoC_SP_Cravinhos'
+        d2 = 'SOC_Received'
+
+        page.goto("https://spx.shopee.com.br/#/dashboard/facility-soc/historical-data", timeout=60000)
+
+        # Campo 1
+        page.wait_for_selector('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[6]/form/div[8]/div/span/span[1]/div/div/div/span/input', timeout=45000)
+        input1 = page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[6]/form/div[8]/div/span/span[1]/div/div/div/span/input')
+        input1.click()
+        input1.fill("")
+        input1.fill(d1)
+        time.sleep(2)
+        page.locator('xpath=/html/body/span[6]/div/div/div/ul/div/div/li').click()
+        time.sleep(2)
+        page.locator('xpath=/html/body/div[1]/div/div[2]/div[1]/div[1]/span[2]/span[1]/span').click()
+
+        # Campo 2
+        input2 = page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[6]/form/div[21]/div/span/span[1]/div/div/div/span/input')
+        input2.click()
+        input2.fill("")
+        input2.fill(d2)
+        time.sleep(2)
+        page.locator('xpath=/html/body/span[4]/div/div/div/ul/li[1]').click()
+        time.sleep(2)
+        page.locator('xpath=/html/body/div[1]/div/div[2]/div[1]/div[1]/span[2]/span[1]/span').click()
+
+        # Botão pesquisar
+        page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[6]/form/div[25]/button[1]').click()
+        time.sleep(4)
+
+        # Captura do dado
+        first_value = page.inner_text('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[10]/div/div[3]/div/span[1]')
         data.append(first_value)
-     
 
     except Exception as e:
         print(f"Erro ao coletar dados: {e}")
         raise
+
     return data
 
-def update_google_sheets(data):
-    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+def update_google_sheets(first_value):
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
     creds = ServiceAccountCredentials.from_json_keyfile_name("hxh.json", scope)
     client = gspread.authorize(creds)
-    sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1R1Ywt_8SuT3X154l1dS30NJEP-JZgoWzH-oq4bOJld0/edit?gid=0#gid=0').worksheet("Python")
 
-    # Copiar o valor da célula AH5 para B61
-    value_to_copy = report_sheet.acell('AE5').value
-    report_sheet.update('B55', [[value_to_copy]])  # Atualiza B61 com o valor da célula AH5
+    sheet = client.open_by_url(
+        'https://docs.google.com/spreadsheets/d/1R1Ywt_8SuT3X154l1dS30NJEP-JZgoWzH-oq4bOJld0/edit'
+    ).worksheet("Reporte HxH")
 
+    sheet.update('B55', [[first_value]])
 
 def main():
     with sync_playwright() as p:
@@ -63,9 +92,11 @@ def main():
         try:
             login(page)
             data = get_data(page)
-            update_google_sheets(data)
-            print("Dados atualizados com sucesso.")
-
+            if data:
+                update_google_sheets(data[0])
+                print("Dados atualizados com sucesso.")
+            else:
+                print("Nenhum dado encontrado.")
         except Exception as e:
             print(f"Erro durante o processo: {e}")
         finally:
